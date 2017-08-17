ea56c1f8c77ad60f51fa646a3badee43ed2ffc10
takumi kimura <juventus0901@gmail.com>
2017-08-17 18:14:05 +0900

    logger

--- client/app/bundles/App/redux/ConfigureStore.js
+++ client/app/bundles/App/redux/ConfigureStore.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import RootReducer from './RootReducer'
 import { ApolloClient } from 'react-apollo'
+import { createLogger } from 'redux-logger'
 const client = new ApolloClient()
 
 const configureStore = (railsProps) => (
-  createStore(RootReducer, railsProps, compose(applyMiddleware(client.middleware())))
+  createStore(RootReducer, railsProps, compose(applyMiddleware(client.middleware(), createLogger())))
 )
 
 export default configureStore
